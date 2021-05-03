1d39267c4b772b05e60c220b747f8820f51f3c2d
Robin Blom <robin.blom@hotmail.se>
2021-05-03 10:30:35 +0200

    2.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -54,5 +54,31 @@ namespace MatchGame
 
 
         }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        public void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)         // Metoden anropas vid musklick. Den tar två in-parametrar. Dels emojin användaren klickar på. Den andra parametern är för själva musklicket. 
+        {
+            TextBlock textBlock = sender as TextBlock;                                  // Den emoji som användaren klickar på hamnar i "textBlock". 
+            if (findingMatch == false)                                                  // OM findingMatch är false (om användaren ej har matchat två emojis). Så gör det här... 
+            {
+                textBlock.Visibility = Visibility.Hidden;                               // Sätt emojin som användaren klickade på till osynlig (och oklickbar). 
+                lastTextBlockClicked = textBlock;                                       // Vi lagrar emojin som användaren klickade på i variabeln "lastTextBlockClicked" (ifall den behöver visas igen, om nästa emoji användaren klickar på inte är samma).
+                findingMatch = true;                                                    // Sätt sedan vår bool "findingMatch" till true. Detta gör att vid nästa musklick hoppar programmet över IF-satsen och går direkt till else if-satsen istället. 
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)                       // Programmet kontrollerar om den första emojin användaren klickade på är samma som den emoji användaren nyss klickade på..     (MATCHNING)
+            {
+                textBlock.Visibility = Visibility.Hidden;                               // Sätt den senaste klickade emojin till osynlig (och oklickbar).
+                findingMatch = false;                                                   // Eftersom vi nu har hittat en matchning ska vår bool återgå till false, så att programmet i nästa varv går in i IF-satsen ovan. 
+            }
+            else                                                                        // ANNARS om användaren klickar på en emoji som ej matchar den första emojin...
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;                   // Programmet sätter den första klickade emojin till att bli synlig igen (så att det går att klicka på den igen under nästa runda).
+                findingMatch = false;                                                   // Vi sätter vår bool "findingMatch" till false, så att programmet under nästa runda går in i IF-satsen ovan. 
+            }
+
+
+        }
     }
 }
